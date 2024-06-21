93764f988537eae9c6e0fe33563d98a973970afc
TemitopeAsama <temitopeasama@gmail.com>
2024-06-21 13:04:23 +0100

    Page-by-page SEO implementation

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,11 +1,22 @@
 'use client';
+
+import { SEO } from '@/seo';
 import { sendGAEvent } from '@next/third-parties/google';
+
+const metadata = {
+  title: 'Coming Soon',
+  description: 'DevFest Lagos comes up on...',
+  keywords: 'devfest, devfest lagos, coming soon, date, venue',
+};
 
 const GAEvent = () => {
   return (
-    <button onClick={() => sendGAEvent({ event: 'buttonClicked', value: 'xyz' })}>
-      This button sends a GA event
-    </button>
+    <>
+      <SEO title={metadata.title} description={metadata.description} keywords={metadata.keywords} />
+      <button onClick={() => sendGAEvent({ event: 'buttonClicked', value: 'xyz' })}>
+        This button sends a GA event
+      </button>
+    </>
   );
 };
 
---
+++ src/seo.tsx
@@ -0,0 +1,46 @@
+import React, { FC } from 'react';
+import Head from 'next/head';
+
+type Props = {
+  title: string;
+  description: string;
+  keywords?: string;
+  image?: string;
+  tags?: Array<{
+    name: string;
+    content: string;
+  }>;
+};
+
+export const SEO: FC<Props> = ({
+  title: _title,
+  description,
+  keywords = 'gdg lagos, devfest, devfest lagos, devfest lagos 2023',
+  image = '/og-images/home-page.png',
+  tags = [],
+}) => {
+  const title = `Devfest Lagos 2024 | ${_title}`;
+
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='twitter:title' content={title} />
+      <meta name='twitter:description' content={description} />
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={description} />
+      <meta property='og:type' content='website' />
+      {image && (
+        <>
+          <meta name='twitter:image' content={image} />
+          <meta property='og:image' content={image} />
+        </>
+      )}
+      {keywords && <meta name='keywords' content={keywords} />}
+      {tags.map((tag, index) => (
+        <meta key={index} name={tag.name} content={tag.content} />
+      ))}
+    </Head>
+  );
+};
