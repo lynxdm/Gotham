7dd0a287bff32d401864adb13565effa2cac7dc6
TemitopeAsama <temitopeasama@gmail.com>
2024-06-21 11:54:18 +0100

    google analytics config rework

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
-import React, { Suspense } from 'react';
-// import { usePathname, useSearchParams } from 'next/navigation';
-import Script from 'next/script';
-import * as gtag from '../lib/google-analytics/ga';
+import React from 'react';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import ReactQueryProvider from '@/lib/reactQuery/providers';
 import './globals.css';
 import { Metadata } from 'next';
@@ -13,48 +11,15 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  // const pathname = usePathname();
-  // const searchParams = useSearchParams();
-
-  // effect for google analytics to track page change
-  // React.useEffect(() => {
-  //   const handleRouteChange = (url: string) => {
-  //     gtag.pageview(url);
-  //   };
-  //   handleRouteChange(pathname);
-  // }, [pathname, searchParams]);
-
   return (
     <html lang='en'>
-      <head>
-        <link rel='icon' href='/favicon.svg' type='image/svg+xml' />
-        <link rel='alternate icon' href='/favicon.ico' type='image/x-icon' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-      </head>
-      <Script
-        strategy='afterInteractive'
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id='gtag-init'
-        strategy='afterInteractive'
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
       <body>
         <ReactQueryProvider>
-          {/* Your layout content, including header, main content, footer, etc. */}
-          <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+          {/* Your layout content, including header, main content, footer, etc goes here. */}
+          {children}
         </ReactQueryProvider>
       </body>
+      <GoogleAnalytics gaId={`${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
     </html>
   );
 }
