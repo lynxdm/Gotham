33b51ace2ffcc1c6137c4a06c351baffc06d28f1
timmy471 <ayantunjitimilehin@gmail.com>
2024-06-29 01:22:02 +0100

    FEATURE: Countdown timer

---
+++ src/components/home/CountdownTimer.tsx
@@ -0,0 +1,100 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+const isValidDateFormat = (dateString: string) => {
+  const regex = /^\d{2}\/\d{2}\/\d{4} \d{2}:\d{2}:\d{2}$/;
+  return regex.test(dateString);
+};
+
+const parseDateString = (dateString: string) => {
+  if (!isValidDateFormat(dateString)) {
+    console.error(
+      'Invalid date format. Please use "DD/MM/YYYY HH:MM:SS". e.g: 29/10/2024 00:35:00',
+    );
+
+    dateString = '00/00/0000 00:00:00';
+  }
+
+  const [datePart, timePart] = dateString.split(' ');
+  const [day, month, year] = datePart.split('/');
+  const [hours, minutes, seconds] = timePart.split(':');
+
+  return new Date(
+    parseInt(year),
+    parseInt(month) - 1,
+    parseInt(day),
+    parseInt(hours),
+    parseInt(minutes),
+    parseInt(seconds),
+  );
+};
+
+const calculateTimeLeft = (eventDate: Date) => {
+  const difference = +eventDate - +new Date();
+  let timeLeft = {
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  };
+
+  if (difference > 0) {
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      minutes: Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
+      seconds: Math.floor((difference % (1000 * 60)) / 1000),
+    };
+  }
+
+  return timeLeft;
+};
+
+export const CountdownTimer = () => {
+  const eventDateStr = process.env.NEXT_PUBLIC_EVENT_TIME || '';
+  const eventDate = parseDateString(eventDateStr);
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(eventDate));
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const newTimeLeft = calculateTimeLeft(eventDate);
+      setTimeLeft(newTimeLeft);
+
+      if (
+        newTimeLeft.days === 0 &&
+        newTimeLeft.hours === 0 &&
+        newTimeLeft.minutes === 0 &&
+        newTimeLeft.seconds === 0
+      ) {
+        clearInterval(timer);
+      }
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [eventDate]);
+
+  const { days, hours, minutes, seconds } = timeLeft;
+
+  return (
+    <div className='countdown-container'>
+      <div>
+        <h1 className='text-6xl'>{days}</h1>
+        <p>Days</p>
+      </div>
+      <div>
+        <h1 className='text-6xl'>{hours}</h1>
+        <p>Hours</p>
+      </div>
+      <div>
+        <h1 className='text-6xl'>{minutes}</h1>
+        <p>Minutes</p>
+      </div>
+      <div>
+        <h1 className='text-6xl'>{seconds}</h1>
+        <p>Seconds</p>
+      </div>
+    </div>
+  );
+};
--- src/components/home/HomeBanner.tsx
+++ src/components/home/HomeBanner.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { Button } from '../shared';
+import { CountdownTimer } from './CountdownTimer';
 
 export const HomeBanner = () => {
   return (
@@ -9,6 +10,17 @@ export const HomeBanner = () => {
           <Image src={'/images/svg/devfest-logo.svg'} alt='DevFest Lagos' height='36' width='111' />
           <Button label='Notify me' size='sm' />
         </div>
+        <div className='hero-text-container'>
+          <h2 className='text-5xl'>DevFest Lagos is Back, Bigger and Better</h2>
+          <p className='text-xl'>
+            We&apos;re back! and it&apos;s about to be the biggest and most unforgettable tech
+            festival yet. Get ready for DevFest Lagos 2024.
+          </p>
+        </div>
+        <div className='text-center'>
+          <Button label='Get Early Bird Tickets' size='lg' />
+        </div>
+        {process.env.NEXT_PUBLIC_EVENT_TIME ? <CountdownTimer /> : ''}
       </div>
     </section>
   );
